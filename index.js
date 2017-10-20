7b06b41f7782a6f134a5ecf0b942faf67bde1c76
m8r1x <m8r1x.io@gmail.com>
2017-10-20 19:07:22 +0300

    add server methods for authentication module

--- index.js
+++ index.js
@@ -28,7 +28,68 @@ server.method('findByName', (model, name, next) => {
   });
 });
 
+server.method('registerUser', (model, payload, next) => {
+
+  model.findOne({ email: payload.email }, (err, user) => {
+
+    if (err) {
+      next(err);
+    }
+    if (user) {
+      if (user.email === payload.email) {
+        return next(err, { message: 'email exists.' });
+      }
+    }
+    const new_user = new model(payload);
+    new_user.save();
+    return next(err, { message: 'Registration successful.' });
+  });
+});
+
+server.method('fetchToken', (model, payload, next) => {
+
+  model.findOne({ email: payload.email }).exec((err, user) => {
+
+    if (err) {
+      next(err);
+    }
+    if (!user) {
+      return next(err, { message: 'Not found' });
+    }
+    user.comparePassword(payload.password, (err, authorized) => {
+
+      if (err) {
+        next(err);
+      }
+      if (!authorized) {
+        return next(err, { message: 'Unauthorized' });
+      }
+      const token = require('jsonwebtoken').sign({
+        user: require('lodash').pick(user, ['_id', 'email'])
+      }, '3j5OjwA63Bo3WdZFnChCnDuF3PSHtwIkLmNykBjdtXM=',
+      { expiresIn: '1h', algorithm: 'HS256' });
+      return next(err, token);
+    });
+  });
+});
+
+server.method('validateUser', (email, next) => {
+
+  require('./lib/modules/authentication/model').findOne({
+    email }, { email : 1 })
+    .exec((err, user) => {
+
+      if (err) {
+        return next(err);
+      }
+      next(err, user);
+    });
+});
+
 server.register([
+  {
+    register: require('hapi-auth-jwt')
+  },
   {
     register: require('./lib/plugins/util')
   },
@@ -47,6 +108,12 @@ server.register([
     register: require('./lib/modules/companies'),
     options: {
       baseUrl: '/v1/companies'
+    }
+  },
+  {
+    register: require('./lib/modules/authentication'),
+    options: {
+      baseUrl: '/v1/user'
     }
   }
 ], (err) => {
